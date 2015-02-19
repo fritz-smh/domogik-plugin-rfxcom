4b39882cbd2c463ed0a0ed79aba54c17aedc8e8f
Fritz <fritz.smh@gmail.com>
2015-02-19 22:36:00 +0100

    Fix type 11 on xpl-cmnd msg received

--- bin/rfxcom.py
+++ bin/rfxcom.py
@@ -144,7 +144,7 @@ class RfxcomManager(XplPlugin):
         trig_msg = message
         trig_msg.set_type("xpl-trig")
         trig_msg.set_target("*")
-        trig_msg.set_source(self.source)
+        trig_msg.set_source(message.source)
         # Use the rfxcom
         if self.rfxcom_manager.command_11(address, unit, command, level, eu, group, trig_msg):
             self.myxpl.send(trig_msg)
